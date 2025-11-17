15efde77852f69eee0953cf11a6779bb786ec5e0
michaelong7 <michael.ong@mail.utoronto.ca>
2025-11-17 15:53:07 -0500

    chnaged version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='autograder',
-    version='0.1.1',    
+    version='0.2.0',    
     description='For use in automatically grading and providing feedback for problems in Jupyter notebooks.',
     url='https://github.com/michaelong7/autograder',
     packages=['autograder'],
